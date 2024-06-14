003315df827da5b9f46223ecbcf1236531867aba
PrabhjotSodhi <sodhiprabhjot23@gmail.com>
2024-06-14 12:14:39 +1200

    update server.js src folder

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+// Serve static files from the 'src' directory
+app.use(express.static('src'));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 // Serve dietary requirements JSON
 app.get('/dietary', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dietary.json'));
+    res.sendFile(path.join(__dirname, 'src', 'dietary.json'));
 });
 
 app.get('/search', async (req, res) => {
